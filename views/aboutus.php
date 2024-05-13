44144d47b5044e9342109bf31df9957a163038ae
rayan-lagmouch <rayan@360dayz.com>
2024-05-13 13:15:50 +0200

    verandering

--- views/aboutus.php
+++ views/aboutus.php
@@ -1,6 +1,47 @@
 <?php require_once __DIR__ . '/partials/_header.php'; ?>
 
+<body> 
 
+    <div class="background-image">
+        <img src="img/BakkerImage.png" alt="BakkerImage">
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6" id="container1">     
+                <h1>Choclatier Academie</h1>
+
+                <h2>Choclatier Academie past bij jou</h2>
+                <p>Bij Choclatier academie geloven we in jou, mag je zijn wie je bent en word je gezien. Het draait om jou!</p>
+
+                <p id=reden>3 redenen om voor Choclatier Academie te kiezen:</p>
+
+                <h3>1. Persoonlijke school & betrokken docenten</h3>
+                <p>Wij zijn een persoonlijke school waar jij je snel thuis zult voelen. Dat komt door onze betrokken docenten die niet alleen aandacht hebben voor de manier waarop jij leert, maar ook voor wie jij bent als persoon.</p>
+
+                <h3>2. Leuke sfeer in een leuke stad</h3>
+                <p>De Choclatier Academie heeft wel 50 verschillende opleidingen op goed bereikbare locaties. Studenten van diverse schoolopleidingen komen elkaar tegen en leren van elkaar. Naast studeren is er ook ruimte voor gezelligheid en ontspanning op school</p>
+
+                <h3>3. Straks een goed opgeleide professional</h3>
+                <p>We vinden het belangrijk dat je naast je opleiding ook vaardigheden</p> <p>leert die je helpen in deze wereld die continu verandert. Daarom leren wij je meer dan alleen een vak. Zo ben je straks een goed opgeleide professional die klaar is om in de praktijk aan de slag te gaan.</p>
+            </div>
+        </div>
+    </div>
+
+     <div class=container2>
+            <div class="row">
+                <div class="col-md-6" id="container2">
+                    <ul>
+                    <li id=loginSch><a href='AcademieInfo.php'></a>Aanmelden opleiding</li>
+                    <li>School</li>
+                    <li>Na School</li>
+                    <li>Sfeer</li>
+                    <li>Volg ons</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </body>
 
 <?php require_once __DIR__ . '/partials/_footer.php'; ?>
 
