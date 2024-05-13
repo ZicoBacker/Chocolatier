9c08ed3da0f4a2a8aad1fcc6e6769461cb808dc9
LorenzoABS <150017251+LorenzoABS@users.noreply.github.com>
2024-05-13 12:39:21 +0200

    begin aan academie

--- app/views/Academie.php
+++ app/views/Academie.php
@@ -4,8 +4,64 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+
+
+<img id = "koekje1" src="IMG/koekjeStart.png">
+
+
+<div class="container">
+        <div class="row">
+            <div class="col-md-6" id="container1">
+<h1>Choclatier Academie</h1>
+
+<h1> <hr> Chocaltier academie Niveau 2 | BOL | 2-3 JR </hr></h1>
+<h2> <hr>Choclatier Academie past bij jou </hr> </h2>
+<p>Streef je naar perfectie in elke chocolade creatie die je maakt? </p>
+<p>Ben je gebeten door de passie voor chocolade en zie je overal mogelijkheden </p>
+<P>om jouw droom als chocolatier waar te maken? Als je ja knikt, dan is onze </p>
+<P>opleiding tot chocolatier jouw ultieme smaakvolle bestemming!</p>
+<p>zijn wie je bent en word je gezien. Het draait om jou!</p>
+
+<h2>De opleiding</h2>
+<p>De opleiding tot choclatier combineert ambacht en ondernemerschap.</p>
+<p>Je leert niet alleen de kunst van chocolade maken, maar ook hoe je</p>
+<p>een bloeiend bedrijf opzet. Een goed doordacht bedrijfsplan staat </p>
+<p>centraal in het curriculum. Je ontwikkelt leiderschapsvaardigheden</p>
+<p>en leert efficiënt voorraadbeheer. Verkoopgesprekken en het opbouwen</p>
+<p>van een klantenkring komen uitgebreid aan bod. Kortom, deze opleiding</p>
+<p>biedt een unieke kans om jouw passie voor chocolade om te zetten in </p>
+<p>een succesvolle onderneming.</p>
+<br>
+<br>
+<hr>
+<h3>wat ga je leren ?</h3>
+<hr>
+<h3>inhoud en vakken</h3>
+<hr>
+<h3>stagelopen</h3>
+<hr>
+<h3>versnellen of vertragen</h3>
+<hr>
+</div>
+</div>
+</div>
+<div class="row">
+<div class="col-md-6" id="container2">
+<h4> Praktish informatie</h4>
+<p>duur</p> <br>
+<p>3 jaar</p> <hr>
+</div>
+</div>
+<img id="Div3" src="IMG/div3.png" alt="">
+
+
+
 </body>
 </html>
+
+<?php
+
+?>
