96d2dea483b6fcfb0bd173aeff53e6b5f17b6f75
EmiledeJong338569 <338569@student.mboutrecht.nl>
2024-05-26 16:22:16 +0200

    footer

---
+++ views/partials/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <footer id= "footer">
+        <img id= "koekie" src="Schermafbeelding 2024-04-25 091806.png" alt="">
+    <ul>
+<li id = "home"><a href="Home">Home</a></li>
+<li id = "Opleiding"><a href="Info Opleiding">Info Opleiding</a></li>
+<li id = "School"><a href="Info School">Info School</a></li>
+   </ul> 
+   <p id = "adres">Adres: Daltonlaan 300 <br> Plaats: Utrecht <br> Tel: 030 243 234 1</p>
+   
+    <img id = "insta" src="Instagram_logo_2016.svg.webp" alt="">
+   <img id = "facebook" src="53cbb8b28229026510b3afd3b24577a9.png" alt="">
+   <img id = "tiktok" src="black-tiktok-logo.png" alt="">
+   
+   
+</footer>
+
+</body>
+</html>
+
+<?php
+
+?>
