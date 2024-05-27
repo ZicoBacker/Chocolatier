40937e7160103fd03aca5fba367831806dec195f
rayan-lagmouch <rayan@360dayz.com>
2024-05-27 10:34:15 +0200

    ada

--- views/partials/_nav.php
+++ views/partials/_nav.php
@@ -1,23 +1,23 @@
 <header>
     <nav>
-        <ul class='nav-bar'>
-            <li class='logo'><a href='/Game'><img src='/img/logo-nav.png'/></a></li>
-            <input type='checkbox' id='check' />
-            <span class="menu">
-                <li><a class="Links" href="/">Home</a></li>
-                <li><a class="Links" href="/Academie">Info Academie</a></li>
-                <li><a class="Links" href="/AboutUs">Info School</a></li>
-                <li><a class="button-nav" href="/Form">Inschrijven</a></li>
-                <label for="check" class="close-menu"><i class="fas fa-times"></i></label>
+        <ul class="nav-bar flex justify-between items-center fixed top-0 w-full bg-custom-beige p-0 z-50">
+            <li class="logo"><a href="/Game"><img src="/img/logo-nav.png" class="w-4/5 h-4/5 p-1" /></a></li>
+            <input type="checkbox" id="check" class="hidden" />
+            <span class="menu flex p-2 fixed top-0 right-0 h-screen w-3/4 bg-custom-brown transition-all duration-200 ease-in-out z-40 md:relative md:h-auto md:w-auto md:flex-row md:bg-transparent md:static md:flex">
+                <li class="mt-10 md:mt-0 md:ml-28"><a class="Links text-[#2B1A12] uppercase relative after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-0 after:h-[1px] after:bg-[#2B1A12] after:transition-all duration-200 ease-in-out hover:after:w-full" href="/">Home</a></li>
+                <li class="mt-10 md:mt-0 md:ml-28"><a class="Links text-[#2B1A12] uppercase relative after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-0 after:h-[1px] after:bg-[#2B1A12] after:transition-all duration-200 ease-in-out hover:after:w-full" href="/Academie">Info Academie</a></li>
+                <li class="mt-10 md:mt-0 md:ml-28"><a class="Links text-[#2B1A12] uppercase relative after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-0 after:h-[1px] after:bg-[#2B1A12] after:transition-all duration-200 ease-in-out hover:after:w-full" href="/About">Info School</a></li>
+                <li class="mt-10 md:mt-0 md:ml-28"><a class="button-nav bg-custom-orange text-black uppercase transition-all duration-150 ease-in-out rounded py-2 px-4 hover:bg-custom-hover-orange" href="/Form">Inschrijven</a></li>
+                <label for="check" class="close-menu absolute top-5 right-5 text-black cursor-pointer text-2xl md:hidden"><i class="fas fa-times"></i></label>
             </span>
-            <label for="check" class="open-menu"><i class="fas fa-bars"></i></label>
+            <label for="check" class="open-menu absolute top-1/2 right-5 transform -translate-y-1/2 text-black cursor-pointer text-2xl md:hidden"><i class="fas fa-bars"></i></label>
         </ul>
     </nav>
-  </header>
-
+</header>
 
 <?php if (!empty($flashMessages)): ?>
-    <div class="alert alert-<?= htmlspecialchars($flashMessages['type']); ?> text-center mt-5" role="alert" style="width: 300px; margin: 0 auto; color: green;">
+    <div class="alert alert-<?= htmlspecialchars($flashMessages['type']); ?> text-center mt-5 mx-auto w-72 text-green-700">
         <p><?= htmlspecialchars($flashMessages['message']); ?></p>
     </div>
 <?php endif; ?>
+ 
